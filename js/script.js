dc92bbf995d1e03928b35afe0f3197e7441fc0b1
zweruga <maratnasyrov97@gmail.com>
2024-02-06 00:07:40 +0300

    finish full map section

--- js/script.js
+++ js/script.js
@@ -182,4 +182,72 @@ document.addEventListener("DOMContentLoaded", () => {
 
     reviewsInner.style.transform = `translateX(${(offset -= 403)}px)`;
   }
+
+  // YANDEX MAP
+  function init() {
+    const map = new ymaps.Map("map", {
+      center: [55.74797656898648, 37.627220499999915],
+      zoom: 17,
+      controls: ["zoomControl", "fullscreenControl"],
+    });
+
+    const myPlacemark = new ymaps.Placemark(
+      map.getCenter(),
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "img/logo/logo-map.png",
+        iconImageSize: [62, 63],
+      }
+    );
+
+    map.geoObjects.add(myPlacemark);
+    map.behaviors.disable(["drag", "scrollZoom", "dblClickZoom", "multiTouch"]);
+
+    function closeDesktopBalloon() {
+      document.querySelector("#desktop-balloon").style.display = "none";
+    }
+
+    function openBalloon() {
+      map.balloon.open(
+        [55.74797656898648, 37.627220499999915],
+        `
+          <div class="balloon balloon_mobile">
+            <div class="item-title balloon__title">г. Москва</div>
+            <address class="item-descr balloon__address">
+              ул. Садовническая, дом 5, офис 4-6 700 от м. Новокузнецкая
+              <br />
+              Тел: +7 (926) 423 01 00
+            </address>
+            <div class="balloon__link">
+              <a href="mailto:info@glopt.ru">info@glopt.ru</a>
+            </div>
+          </div>
+        `,
+        {
+          closeButton: false,
+          offset: [15, -50]
+        }
+      );
+    }
+
+    const mediaQueryMap = window.matchMedia("(max-width: 800px)");
+
+    if (mediaQueryMap.matches) {
+      openBalloon();
+      closeDesktopBalloon();
+    }
+
+    mediaQueryMap.addEventListener("change", () => {
+      if (mediaQueryMap.matches) {
+        openBalloon();
+        closeDesktopBalloon();
+      } else {
+        map.balloon.close();
+        document.querySelector("#desktop-balloon").style.display = "block";
+      }
+    });
+  }
+
+  ymaps.ready(init);
 });
